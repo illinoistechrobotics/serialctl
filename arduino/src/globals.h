c85fcb19e8e19dbc8e37af42f4578e82ca427b6a
Peter Chinetti <peter@chinetti.me>
2015-03-22 04:11:17 -0500

    working fenrir code!

--- arduino/src/globals.h
+++ arduino/src/globals.h
@@ -6,12 +6,12 @@ extern packet_t *astate, *incoming;
 extern comm_state cs;
 extern long last_p;
 extern int speed;
-#define SerComm Serial
+#define SerComm Serial2
 #define htons(x) ( ((x)<<8) | (((x)>>8)&0xFF) )
 #define ntohs(x) htons(x)
 #define htonl(x) ( ((x)<<24 & 0xFF000000UL) | ((x)<< 8 & 0x00FF0000UL) | ((x)>> 8 & 0x0000FF00UL) | ((x)>>24 & 0x000000FFUL) )
 #define ntohl(x) htonl(x)
-#define DEADBAND_HALF_WIDTH 5
+#define DEADBAND_HALF_WIDTH 0
 #define SPINNER_PIN 2
 #define ARM_PIN 3
 #define WINCH_PIN 4
--- arduino/src/hw.ino
+++ arduino/src/hw.ino
@@ -78,41 +78,19 @@ void tank_drive(){
       turn_out = zeroed_turn + DEADBAND_HALF_WIDTH;
     }
   }
-  int left_out =     power_out + (turn_out/8);
-  int right_out = -1*power_out + (turn_out/8);
 
-  //apply turbo mode
-  if(getButton(6)){
-    if(abs(power_out)> 75){
-      left_out  =    power_out*2 + (turn_out);
-      right_out = -1*power_out*2 + (turn_out);
-    } else if(abs(power_out) >  20){
-      left_out  =    power_out*2 + (turn_out/4);
-      right_out = -1*power_out*2 + (turn_out/4);
-    } else {
-      left_out  =    power_out + (turn_out);
-      right_out = -1*power_out + (turn_out);
-    }
-  } else if(getButton(4)){ //precision mode
-    if(abs(power_out)> 75){
-      left_out  =    power_out/2 + (turn_out/2);
-      right_out = -1*power_out/2 + (turn_out/2);
-    } else if(abs(power_out) >  20){
-      left_out  =    power_out/2 + (turn_out/8);
-      right_out = -1*power_out/2 + (turn_out/8);
-    } else {
-      left_out  /= 2;
-      right_out /= 2;
-    }
-  } else {
-    if(abs(power_out)> 75){
-      left_out  =    power_out + (turn_out);
-      right_out = -1*power_out + (turn_out);
-    } else if(abs(power_out) >  20){
-      left_out  =    power_out + (turn_out/4);
-      right_out = -1*power_out + (turn_out/4);
-    }
+  int multiplier;
+  if(getButton(6)){ //turbo
+    multiplier = 4;
   }
+  else if(getButton(4)){ //precise
+    multiplier = 1;
+  } else {
+    multiplier = 2;
+  }
+
+  int left_out =        multiplier*(power_out - (turn_out));
+  int right_out =    -1*multiplier*(power_out + (turn_out));
 
   drive_left(left_out);
   drive_right(right_out);
