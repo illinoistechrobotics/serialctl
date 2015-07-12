708bf5d513a4c380c3932afda3b09d097d2d057f
Nash Kaminski <nashkaminski@comcast.net>
2015-07-12 15:00:31 -0400

    add missing curly braces in init

--- main.c
+++ main.c
@@ -7,46 +7,49 @@
 #include <arpa/inet.h>
 #include <time.h>
 void packet_crc(packet_t *p){
-    p->cksum = htons(compute_crc((char *)p,sizeof(packet_t)-sizeof(uint16_t)));
-    }
+        p->cksum = htons(compute_crc((char *)p,sizeof(packet_t)-sizeof(uint16_t)));
+}
 
 int main(int argc, char ** argv){
         char msg[RECVBUF];
         if(argc != 3){
-            printf("Usage: ./serialctl <serial port> <joystick_num>\n");
-            return 3;
-            }
+                printf("Usage: ./serialctl <serial port> <joystick_num>\n");
+                return 3;
+        }
         short loop=1;
         connection_t c;
         packet_t ctl;
-        if(serio_init(&c, argv[1]))
-	  printf("Unable to open serial port");
-            return 2;
-        if(joystick_init(atoi(argv[2])) != 0)
-	  printf("Unable to open joystick");
-          return 1;
-        if(joystick_wait_safe() != 0)
-	  printf("Unable to get safe values from the joystick");
-          return 1;
-	init_ui();
+        if(serio_init(&c, argv[1]) !=0 ){
+                printf("Unable to open serial port");
+                return 2;
+        }
+        if(joystick_init(atoi(argv[2])) != 0){
+                printf("Unable to open joystick");
+                return 1;
+        }
+        if(joystick_wait_safe() != 0){
+                printf("Unable to get safe values from the joystick");
+                return 1;
+        }
+        init_ui();
         while(loop){
-	  int overflow = 0;
-	  //TODO: printf("Input buffer overflow!\n");
-          if(serio_recv(&c, msg, &overflow) < 0){
-            printf("Error reading data!\n");
-            return 2;
-            }
-        if(joystick_update(&ctl) != 0){
-           return 1;
-            }
-        packet_crc(&ctl); 
-        if(serio_send(&c, &ctl, sizeof(packet_t)) < 0){
-            printf("Unable to send data!\n");
-            return 2;
-            }
-        //printf("X: %i, Y: %i, CRC: %i, Resp: %s\n", ctl.stickX, ctl.stickY, ctl.cksum, msg); 
-	refresh_ui(&ctl, msg, overflow);
-//      usleep(150E3);
+                int overflow = 0;
+                //TODO: printf("Input buffer overflow!\n");
+                if(serio_recv(&c, msg, &overflow) < 0){
+                        printf("Error reading data!\n");
+                        return 2;
+                }
+                if(joystick_update(&ctl) != 0){
+                        return 1;
+                }
+                packet_crc(&ctl); 
+                if(serio_send(&c, &ctl, sizeof(packet_t)) < 0){
+                        printf("Unable to send data!\n");
+                        return 2;
+                }
+                //printf("X: %i, Y: %i, CRC: %i, Resp: %s\n", ctl.stickX, ctl.stickY, ctl.cksum, msg); 
+                refresh_ui(&ctl, msg, overflow);
+                //      usleep(150E3);
         }
         return 0;
 }
