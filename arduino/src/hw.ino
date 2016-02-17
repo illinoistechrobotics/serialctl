ffb11239139d8ee4fcefae4007770359e1c4b66f
Nash Kaminski <robotics.iit@gmail.com>
2016-02-16 21:15:44 -0600

    It now drives!  Yay!

--- arduino/src/hw.ino
+++ arduino/src/hw.ino
@@ -4,39 +4,35 @@
 #include <Servo.h>
 Servo fl, fr, rl, rr;
 void init_pins(){
-        pinMode(LASER_PIN, OUTPUT);
-        digitalWrite(LASER_PIN,LOW);
-        victor_init();
+	pinMode(LASER_PIN, OUTPUT);
+	digitalWrite(LASER_PIN,LOW);
+	victor_init();
 }
 void print_data(){
+	SerComm.print("ITR Icarus ");
 	SerComm.print(leftMotorDebug);
 	SerComm.print(", ");
-	SerComm.print(rightMotorDebug);
-	SerComm.print(", LeftOut: ");
-	char tmp[10];
-	itoa(leftOutDebug, tmp, 10);
-	SerComm.print(tmp);
-        SerComm.println("ITR Scale Roslund");
+	SerComm.println(rightMotorDebug);
 }
 void victor_init(){
-        fl.attach(FRONT_LEFT);
-        fr.attach(FRONT_RIGHT);
-        rl.attach(REAR_LEFT);
-        rr.attach(REAR_RIGHT);
-        fl.writeMicroseconds(1500);
-        fr.writeMicroseconds(1500);
-        rr.writeMicroseconds(1500);
-        rl.writeMicroseconds(1500);
+	fl.attach(FRONT_LEFT);
+	fr.attach(FRONT_RIGHT);
+	rl.attach(REAR_LEFT);
+	rr.attach(REAR_RIGHT);
+	fl.writeMicroseconds(1500);
+	fr.writeMicroseconds(1500);
+	rr.writeMicroseconds(1500);
+	rl.writeMicroseconds(1500);
 }
 
 void drive_left(int power){
-        power = map(constrain(power,-127,127),-127,127,1000,2000);
-        fl.writeMicroseconds(power);
-        rl.writeMicroseconds(power);
+	power = map(constrain(power,-127,127),-127,127,1000,2000);
+	fl.writeMicroseconds(power);
+	rl.writeMicroseconds(power);
 }
 
 void drive_right(int power){
-        power = map(constrain(power,-127,127),127,-127,1000,2000);
-        fr.writeMicroseconds(power);
-        rr.writeMicroseconds(power);
+	power = map(constrain(power,-127,127),127,-127,1000,2000);
+	fr.writeMicroseconds(power);
+	rr.writeMicroseconds(power);
 }
--- arduino/src/serialctl.ino
+++ arduino/src/serialctl.ino
@@ -40,13 +40,15 @@ Servo arm, feed, spinner;
 #endif
 
 int getButton(int num){
-        if(num<=7){
-                return (astate->btnlo >> num) & 0x01;
-        } else if(num>7 && num <= 15){
-                return (astate->btnhi >> (num - 8)) & 0x01;
-        } else {
-                return 0;
-        }
+	if(num<=7) {
+		return (astate->btnlo >> num) & 0x01;
+	}
+	else if(num>7 && num <= 15){
+		return (astate->btnhi >> (num - 8)) & 0x01;
+	}
+	else {
+		return 0;
+	}
 }
 
 SoftwareSerial left(2,3);
@@ -56,145 +58,149 @@ char leftMotorDebug[5];
 int leftOutDebug;
 
 void setup() {
-  #ifdef WATCHDOG_
-  wdt_enable(WDTO_250MS);  //Set 250ms WDT 
-  wdt_reset();             //watchdog timer reset 
-  #endif
-  //Initialize safe to safe values!!
-  safe.stickX = 127;
-  safe.stickY = 127;
-  safe.btnhi = 0;
-  safe.btnlo = 0;
-  safe.cksum = 0b1000000010001011;
-  SerComm.begin(57600);
-  comm_init();
-  init_pins();
-  laser=0;
-  fire=0;
-  armpos=1000;
-  drive_left(0);
-  drive_right(0);
-  arm.attach(ARM_PIN);
-  arm.writeMicroseconds(armpos);
-  feed.attach(FEED_PIN);
-  feed.writeMicroseconds(2300);
-  spinner.attach(SPINNER_PIN);
-  spinner.writeMicroseconds(1500); 
-//start software serial
- left.begin(115200);
- right.begin(115200);
- //copy safe values over the current state
-  memcpy(astate, &safe, sizeof(packet_t));
-  //Dont send data until we recieve something
-  while(1){
-    if(SerComm.available()){
-        if(SerComm.read()=='[')
-            break;
-        }
-        wdt_reset();
-  }
+	#ifdef WATCHDOG_
+	wdt_enable(WDTO_250MS);  //Set 250ms WDT 
+	wdt_reset();             //watchdog timer reset 
+	#endif
+	//Initialize safe to safe values!!
+	safe.stickX = 127;
+	safe.stickY = 127;
+	safe.btnhi = 0;
+	safe.btnlo = 0;
+	safe.cksum = 0b1000000010001011;
+	SerComm.begin(57600);
+	comm_init();
+	init_pins();
+	laser=0;
+	fire=0;
+	armpos=1000;
+	drive_left(0);
+	drive_right(0);
+	arm.attach(ARM_PIN);
+	arm.writeMicroseconds(armpos);
+	feed.attach(FEED_PIN);
+	feed.writeMicroseconds(2300);
+	spinner.attach(SPINNER_PIN);
+	spinner.writeMicroseconds(1500); 
+	// Start software serial
+	left.begin(115200);
+	right.begin(115200);
+	// Copy safe values over the current state
+	memcpy(astate, &safe, sizeof(packet_t));
+	// Set the motors to those safe values
+	tank_drive();
+	// Dont send data until we recieve something
+	while(1){
+		if(SerComm.available()){
+			if(SerComm.read()=='[')
+			break;
+		}
+		wdt_reset();
+	}
 }
+
 void fire_fsm(){
-   if(fire==0){
-      spinner.writeMicroseconds(1500);
-      return;
-   }
-   //Increase state counter
-   fire++;
-   //Start spinners
-   spinner.writeMicroseconds(1700);
-   //T = 1 second, FIRE!!
-   if(fire == (1000/TICK_RATE)){
-      feed.write(0);
-   } else if(fire == (1750/TICK_RATE)){
-   //T = 1.5 seconds, retract and spin down
-      feed.write(180);
-   } else if(fire == (2500/TICK_RATE)){
-      //Reset
-      fire=0;
-      spinner.writeMicroseconds(1500);
-   }
+	if(fire==0){
+		spinner.writeMicroseconds(1500);
+		return;
+	}
+	//Increase state counter
+	fire++;
+	//Start spinners
+	spinner.writeMicroseconds(1700);
+	//T = 1 second, FIRE!!
+	if(fire == (1000/TICK_RATE)) {
+		feed.write(0);
+	}
+	else if(fire == (1750/TICK_RATE)) {
+		//T = 1.5 seconds, retract and spin down
+		feed.write(180);
+	}
+	else if(fire == (2500/TICK_RATE)) {
+		//Reset
+		fire=0;
+		spinner.writeMicroseconds(1500);
+	}
 }
 
 void loop(){
-  //Every line sent to the computer gets us a new state
-  wdt_reset();
-  print_data();
-  comm_parse();
-  if(getButton(7)){
-    //arm up
-    armpos+=2;
-  }
-if(getButton(5)){
-    //arm down
-    armpos-=2;
-  }
-//Limit value before setting
-armpos = constrain(armpos,1000,2000);
-arm.writeMicroseconds(armpos);
+	//Every line sent to the computer gets us a new state
+	wdt_reset();
+	print_data();
+	comm_parse();
+	if(getButton(7)) {
+		//arm up
+		armpos+=2;
+	}
+	if(getButton(5)) {
+		//arm down
+		armpos-=2;
+	}
+	//Limit value before setting
+	armpos = constrain(armpos,1000,2000);
+	arm.writeMicroseconds(armpos);
 
-if(getButton(1) && fire==0){
-  fire=1;
+	if(getButton(1) && fire==0){
+		fire=1;
+	}
+	fire_fsm();
+	if(getButton(3) && (millis()-last_s > 500)){
+		//laser
+		last_s=millis();
+		laser=(laser+1)%2;
+		digitalWrite(LASER_PIN,laser);
+	}
+
+	tank_drive();
+
+	
+	//limits data rate
+	delay(TICK_RATE);
 }
-fire_fsm();
-if(getButton(3) && (millis()-last_s > 500)){
-    //laser
-    last_s=millis();
-    laser=(laser+1)%2;
-    digitalWrite(LASER_PIN,laser);
-  }
 
- tank_drive();
-
-  
-  //limits data rate
-  delay(TICK_RATE);
-}
 void tank_drive(){
-  int power_out = 0;
-  int turn_out  = 0;
-  int zeroed_power =    ((int)(astate->stickX) - 127);
-  int zeroed_turn =     -1*((int)(astate->stickY) - 127);
-  
-  if(abs(zeroed_power) > DEADBAND_HALF_WIDTH){
-    if(zeroed_power>0){
-      power_out = zeroed_power - DEADBAND_HALF_WIDTH;
-    } else {
-      power_out = zeroed_power + DEADBAND_HALF_WIDTH;
-    }
-  }
-  if(abs(zeroed_turn) > DEADBAND_HALF_WIDTH){
-    if(zeroed_turn>0){
-      turn_out = zeroed_turn - DEADBAND_HALF_WIDTH;
-    } else {
-      turn_out = zeroed_turn + DEADBAND_HALF_WIDTH;
-    }
-  }
-  int left_out =     (power_out + (turn_out/2))/2;
-  int right_out = (power_out - (turn_out/2))/2;
- 
-  leftOutDebug = left_out;
-  write_serial_motors(&left, left_out);
-  //write_serial_motors(&right, right_out);
- 
-  //drive_left(left_out);
-  //drive_right(right_out);
+	int zeroed_power = ((int)(astate->stickX) - 128);
+	int zeroed_turn =  ((int)(astate->stickY) - 128);
+
+	// Square inputs
+	zeroed_power = (zeroed_power * abs(zeroed_power)) / 128;
+	zeroed_turn =  (zeroed_turn * abs(zeroed_turn)) / 128;
+
+	int left_out = (zeroed_power + (zeroed_turn));
+	int right_out = -1 * (zeroed_power - (zeroed_turn));
+
+	leftOutDebug = left_out;
+	write_serial_motors(&left, left_out, '1');
+	write_serial_motors(&right, right_out, '1');
+	// If both motors on one controller are written to write after the other,
+	// the controller will miss the second write.  If you want to disable one side,
+	// uncomment the delay here.
+	// delay(1);
+	write_serial_motors(&left, left_out, '2');
+	write_serial_motors(&right, right_out, '2');
+	// delay(1);
 }
 
-uint8_t bytemap(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max) {
-	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-// Speed is 0 to 255 where 127 is stopped.
-void write_serial_motors(SoftwareSerial *controller, int speed) {
+// Speed is -128 to 127 where 0 is stopped.
+void write_serial_motors(SoftwareSerial *controller, int speed, int motorID) {
 	char controlStr[5];
 	strcpy(controlStr, "1f0\r");
-	controlStr[2] = (uint8_t)map(speed,-128,127,'0','9');
-	controller->println(controlStr);
-	strcpy(leftMotorDebug, controlStr);
-	leftMotorDebug[3] = '-';
-	controlStr[0] = '2';
-	controller->println(controlStr);
-	strcpy(rightMotorDebug, controlStr);
-	rightMotorDebug[3] = '-';
+	controlStr[0] = motorID;
+	if (speed < 0) {
+		controlStr[1] = 'r';
+		speed = -1 * speed;
+	}
+	if (speed > 127) {
+		speed = 127;
+	}
+	controlStr[2] = (uint8_t)map(speed, 0, 127, '0', '9');
+	controller->print(controlStr);
+	if (controller == &left) {
+		strcpy(leftMotorDebug, controlStr);
+		leftMotorDebug[3] = '\0';
+	}
+	else {
+		strcpy(rightMotorDebug, controlStr);
+		rightMotorDebug[3] = '\0';
+	}
 }
