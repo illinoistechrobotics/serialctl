061de9a0d94c18c777bd581df2c2e95afd58ef9b
Nash Kaminski <nashkaminski@comcast.net>
2016-03-07 01:38:01 -0600

    New main loop scheduler, compressor code done

---
+++ arduino/serialctl/compressor.ino
@@ -0,0 +1,33 @@
+#define COMPCTL ST12
+#define COMPPIN 2
+float psi;
+void compressor_ctl(){
+  static char pumping=0;
+  int i;
+  psi=0;
+  for(i=0;i<5;i++){
+    //5x multisampling
+    psi += ADC2PSI(P_SENSOR);
+  }
+  psi = psi/5;
+  if(comm_ok == 1){
+    //Read pressures and pump if needed
+    if(psi < MIN_PRESS){
+      pumping = 1;
+    } else if(psi >= MAX_PRESS){
+      pumping = 0;
+    }
+  } else{
+    //No control link, stop compressor
+    pumping=0;
+  }
+  
+  if(pumping == 1){
+    //Go!
+    ST12.motor(2,127);
+  } else{
+    //Stop!
+    ST12.motor(2,0);
+  }
+}
+
