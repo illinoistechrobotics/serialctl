3c4e09d5f806394f66b50a8ddf664480ec0d0e1f
Nash Kaminski <robotics.iit@gmail.com>
2016-03-08 01:56:40 -0600

    changed to serial1

--- arduino/src/globals.h
+++ arduino/src/globals.h
@@ -6,7 +6,7 @@ extern comm_state cs;
 extern long last_p;
 extern char leftMotorDebug[5], rightMotorDebug[5];
 extern int leftOutDebug;
-#define SerComm Serial
+#define SerComm Serial1
 #define htons(x) ( ((x)<<8) | (((x)>>8)&0xFF) )
 #define ntohs(x) htons(x)
 #define htonl(x) ( ((x)<<24 & 0xFF000000UL) | ((x)<< 8 & 0x00FF0000UL) | ((x)>> 8 & 0x0000FF00UL) | ((x)>>24 & 0x000000FFUL) )
