e28a856bc6ced2f15fa6a833cdf3b14018c0dabf
nkaminski <nashkaminski@comcast.net>
2017-02-23 21:44:41 -0600

    Add pinouts and IIC tester code

---
+++ IIC_test/iic_test.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+
+void setup() {
+  setup_iic();
+  Serial.begin(9600);  // start serial for output
+}
+
+void loop() {
+  double left, right;
+  if(iic_encoder_read(5,&left)){
+    Serial.print("LEFT (RPM): ");
+    Serial.println(left);
+  }
+  if(iic_encoder_read(6,&right)){
+    Serial.print("RIGHT (RPM): ");
+    Serial.println(right);
+  }
+  
+  delay(100);
+}
