7b2b0b7eb5512ed1dc22d54b2a58927e07a734ce
Nash Kaminski <robotics.iit@gmail.com>
2019-03-01 21:05:33 -0600

    Change sequencing pinout

--- arduino/serialctl/sequencing.h
+++ arduino/serialctl/sequencing.h
@@ -2,8 +2,8 @@
 #define LIGHT_SENSOR_TICKS 3
 #define DETECT_THRESHOLD 70 // analogRead returns a number out of 1024
 const int light_sensor_pins[] = {A0, A1, A2, A3};
-const int fire_pins[] = {11, 12, 13, 14};
-int ticks_detected[BUTTONS];
+const int fire_pins[] = {47, 49, 51, 53};
+int ticks_detected[BUTTONS]; 
 
 bool play_game = false;
 
